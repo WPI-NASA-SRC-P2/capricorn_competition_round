56bbcb2068eaba582279aa9bbe2f69e07a0b77d3
Rushab Patil <rdpatil@wpi.edu>
2021-01-30 16:44:16 -0500

    Action Server stuff

---
+++ templates/src/templates_actionlib/src/template_actionServer.cpp
@@ -0,0 +1,71 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+
+//Class containing  Action Server methods
+class actionToBePerformed
+{
+    protected:
+
+    //create a nodehandle to create subscribers and publishers
+    ros::NodeHandle nh;
+
+    //declare a simple action server type object
+    //create a nodehandle before this line otherwise it creates weird errors
+    actionlib::SimpleActionServer<template_actionServer::template_actionMsgAction> action_server_;
+
+    //create a variable to hold the name of the action server node
+    std::string action_name_; 
+
+
+    public:
+
+    //constructor with initialization list
+    actionToBePerformed(std::string name) :
+        action_server_(nh, name, boost::bind(&actionToBePerformed) )
+        action_name(name)
+    {
+        initializeSubsribers();
+        initializePublishers();
+        acion_server_.start();
+    }
+
+
+    private:
+
+    //Initialize Subscribers here
+    void initializeSubscribers(void)
+    {
+        ROS_INFO("Subscribers Initialized");
+    }
+
+    //Initialize Publishers here
+    void initializePublishers(void)
+    {
+        ROS_INFO("Publishers Initialized");
+    }
+
+    // below this line create callback functions for initialized subsribers
+}
+
+
+int main(int argc, char** argv)
+{
+    //Initializing the Action Server Node
+    ros::init(argc, argv, "template_actionServer");
+
+    //Creating an instance of the Action Class
+    actionToBePerformed action_1 
+
+    //Setting the rate
+    ros::Rate rate(0.5)
+
+    int n = 0;
+    while( ros::ok())
+    {
+        ros::spinOnce();
+        n++;
+        rate.sleep();
+    }
+
+
+}
