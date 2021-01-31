e8878f10d3af5cfd3fdbcdf7456e97cdbaa2f0bc
Rushab Patil <rdpatil@wpi.edu>
2021-01-30 19:56:54 -0500

    Action Client

---
+++ templates/src/templates_actionlib/src/template_actionClient.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+
+
+int main(int argc, char **argv)
+{
+    //Initializing the Action Client node
+    ros::init(argc, argv, "template_actionClient");
+
+    //Create the Action Client
+    actionlib::SimpleActionClient<templates_actionServer::templateAction> action_client_("fibonacci", true);
+
+}
