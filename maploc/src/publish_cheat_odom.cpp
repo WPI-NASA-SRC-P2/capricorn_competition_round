e1ce22f1ab21e7937d019c1f5df2cda6d3ae1ea9
MahimanaGIT <mahimnabhatt@gmail.com>
2021-02-05 10:49:22 -0500

    Adding cheat odom and maploc

---
+++ maploc/src/publish_cheat_odom.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <tf/transform_broadcaster.h>
+
+#define UPDATE_HZ 10
+
+int main(int argc, char **argv)
+{
+    std::string node_name = std::string(argv[1]) + "_cheat_odom_publisher";
+    ros::init(argc, argv, node_name);
+    ros::NodeHandle nh;
+    ros::AsyncSpinner spin(1);
+
+    std::string model_name = std::string(argv[1]);
+    std::string topic_name = "/capricorn/" + model_name + "/cheat_odom";
+    // std::string topic_name = "/" + model_name + "_loc/filtered_odometry";
+    std::string robot_frame_name = model_name + "_base_footprint";
+
+    ros::ServiceClient client = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+    ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>(topic_name, 1, true);
+
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+
+    gazebo_msgs::GetModelState req;
+    req.request.model_name = model_name;
+    req.request.relative_entity_name = "heightmap";
+    nav_msgs::Odometry odom_msg;
+    tf::Quaternion quat;
+    odom_msg.header.frame_id = "odom";
+    odom_msg.child_frame_id = robot_frame_name;
+
+    ros::Rate update_rate(UPDATE_HZ);
+
+    while (ros::ok())
+    {
+      if (client.call(req))
+      {
+        transform.setOrigin(tf::Vector3(req.response.pose.position.x, req.response.pose.position.y, req.response.pose.position.z));
+        tf::quaternionMsgToTF(req.response.pose.orientation, quat);
+        transform.setRotation(quat);
+        odom_msg.pose.pose = req.response.pose;
+        odom_msg.twist.twist = req.response.twist;
+        odom_msg.header = req.response.header;
+        odom_pub.publish(odom_msg);
+      }
+      else
+      {
+        std::string filename = std::string(argv[0]);
+        int index = filename.find_last_of('/');
+        std::string input_trace_filename = filename.substr(index + 1);
+        ROS_ERROR("Something went wrong in file %s getting odom from gazebo.", input_trace_filename);
+      }
+      update_rate.sleep();
+    }
+    return 0;
+}
