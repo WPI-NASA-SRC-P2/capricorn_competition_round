c762d4d580b1087125cf58d888b2962674c2a1fe
Eli Benevedes <benevedeselias@gmail.com>
2021-03-05 15:11:02 -0500

    Started updating actionlib server. Currently not working.

--- operations/src/navigation_actionlib_server.cpp
+++ operations/src/navigation_actionlib_server.cpp
@@ -6,6 +6,8 @@
 #include <sensor_msgs/Imu.h>
 #include <actionlib/server/simple_action_server.h>
 
+#include <operations/TrajectoryWithVelocities.h>
+
 #include <utils/common_names.h>
 #include <operations/navigation_algorithm.h>
 #include <operations/NavigationAction.h>  // Note: "Action" is appended
@@ -17,6 +19,8 @@ using namespace COMMON_NAMES;
 ros::Publisher front_left_vel_pub_, front_right_vel_pub_, back_left_vel_pub_, back_right_vel_pub_;
 ros::Publisher front_left_steer_pub_, front_right_steer_pub_, back_left_steer_pub_, back_right_steer_pub_;
 
+// Declared globally so that execute() can access. TODO: probably temporary
+std::string robot_name;
 
 /*******************************************************************************/
 /****************** I N I T I A L I Z E   P U B L I S H E R S ******************/
@@ -27,10 +31,10 @@ ros::Publisher front_left_steer_pub_, front_right_steer_pub_, back_left_steer_pu
  */
 void initVelocityPublisher(ros::NodeHandle &nh, const std::string &robot_name)
 {
-  front_left_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_LEFT_WHEEL + VELOCITY_TOPIC, 1000);  
-  front_right_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_RIGHT_WHEEL + VELOCITY_TOPIC, 1000);  
-  back_left_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_LEFT_WHEEL + VELOCITY_TOPIC, 1000);  
-  back_right_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_RIGHT_WHEEL + VELOCITY_TOPIC, 1000);
+	front_left_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_LEFT_WHEEL + VELOCITY_TOPIC, 1000);  
+	front_right_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_RIGHT_WHEEL + VELOCITY_TOPIC, 1000);  
+	back_left_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_LEFT_WHEEL + VELOCITY_TOPIC, 1000);  
+	back_right_vel_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_RIGHT_WHEEL + VELOCITY_TOPIC, 1000);
 }
 
 /**
@@ -39,10 +43,10 @@ void initVelocityPublisher(ros::NodeHandle &nh, const std::string &robot_name)
  */
 void initSteerPublisher(ros::NodeHandle &nh, const std::string &robot_name)
 {
-  front_left_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_LEFT_WHEEL + STEERING_TOPIC, 1000);
-  front_right_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_RIGHT_WHEEL + STEERING_TOPIC, 1000);
-  back_left_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_LEFT_WHEEL + STEERING_TOPIC, 1000);
-  back_right_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_RIGHT_WHEEL + STEERING_TOPIC, 1000);
+	front_left_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_LEFT_WHEEL + STEERING_TOPIC, 1000);
+	front_right_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + FRONT_RIGHT_WHEEL + STEERING_TOPIC, 1000);
+	back_left_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_LEFT_WHEEL + STEERING_TOPIC, 1000);
+	back_right_steer_pub_ = nh.advertise<std_msgs::Float64>(robot_name + BACK_RIGHT_WHEEL + STEERING_TOPIC, 1000);
 }
 
 /**
@@ -51,8 +55,8 @@ void initSteerPublisher(ros::NodeHandle &nh, const std::string &robot_name)
  */
 void initPublishers(ros::NodeHandle &nh, const std::string &robot_name)
 {
-  initVelocityPublisher(nh, robot_name);
-  initSteerPublisher(nh, robot_name);
+	initVelocityPublisher(nh, robot_name);
+	initSteerPublisher(nh, robot_name);
 }
 
 
@@ -69,30 +73,30 @@ void initPublishers(ros::NodeHandle &nh, const std::string &robot_name)
  */
 void publishMessage(ros::Publisher &publisher, float data)
 {
-  std_msgs::Float64 pub_data;
-  pub_data.data = data;
+	std_msgs::Float64 pub_data;
+	pub_data.data = data;
 
-  publisher.publish(pub_data);
+	publisher.publish(pub_data);
 }
 
 /**
  * @brief Steers the robot wheels for the angles
  * 
  * @param angles Steering angles
-   *                  The vector will be in order:
-   *                  Clockwise from top, starting with FRONT_LEFT
-   * 
-   *          element 0: Front Left Wheel
-   *          element 1: Front Right Wheel
-   *          element 2: Back Right Wheel
-   *          element 3: Back Left Wheel
+	 *                  The vector will be in order:
+	 *                  Clockwise from top, starting with FRONT_LEFT
+	 * 
+	 *          element 0: Front Left Wheel
+	 *          element 1: Front Right Wheel
+	 *          element 2: Back Right Wheel
+	 *          element 3: Back Left Wheel
  */
 void steerRobot(const std::vector<float>& angles)
 {
-  publishMessage(front_left_steer_pub_, angles.at(0));
-  publishMessage(front_right_steer_pub_, angles.at(1));
-  publishMessage(back_right_steer_pub_, angles.at(2));
-  publishMessage(back_left_steer_pub_, angles.at(3));
+	publishMessage(front_left_steer_pub_, angles.at(0));
+	publishMessage(front_right_steer_pub_, angles.at(1));
+	publishMessage(back_right_steer_pub_, angles.at(2));
+	publishMessage(back_left_steer_pub_, angles.at(3));
 }
 
 /**
@@ -102,30 +106,30 @@ void steerRobot(const std::vector<float>& angles)
  */
 void steerRobot(const float angle)
 {
-  publishMessage(front_left_steer_pub_, angle);
-  publishMessage(front_right_steer_pub_, angle);
-  publishMessage(back_right_steer_pub_, angle);
-  publishMessage(back_left_steer_pub_, angle);
+	publishMessage(front_left_steer_pub_, angle);
+	publishMessage(front_right_steer_pub_, angle);
+	publishMessage(back_right_steer_pub_, angle);
+	publishMessage(back_left_steer_pub_, angle);
 }
 
 /**
  * @brief Move robot wheels with the given velocities
  * 
  * @param velocity Wheel Velocities
-   *                  The vector will be in order:
-   *                  Clockwise from top, starting with FRONT_LEFT
-   * 
-   *          element 0: Front Left Wheel
-   *          element 1: Front Right Wheel
-   *          element 2: Back Right Wheel
-   *          element 3: Back Left Wheel
+	 *                  The vector will be in order:
+	 *                  Clockwise from top, starting with FRONT_LEFT
+	 * 
+	 *          element 0: Front Left Wheel
+	 *          element 1: Front Right Wheel
+	 *          element 2: Back Right Wheel
+	 *          element 3: Back Left Wheel
  */
 void moveRobotWheels(const std::vector<float> velocity)
 {
-  publishMessage(front_left_vel_pub_, velocity.at(0));
-  publishMessage(front_right_vel_pub_, velocity.at(1));
-  publishMessage(back_right_vel_pub_, velocity.at(2));
-  publishMessage(back_left_vel_pub_, velocity.at(3));
+	publishMessage(front_left_vel_pub_, velocity.at(0));
+	publishMessage(front_right_vel_pub_, velocity.at(1));
+	publishMessage(back_right_vel_pub_, velocity.at(2));
+	publishMessage(back_left_vel_pub_, velocity.at(3));
 }
 
 /**
@@ -135,13 +139,50 @@ void moveRobotWheels(const std::vector<float> velocity)
  */
 void moveRobotWheels(const float velocity)
 {
-  publishMessage(front_left_vel_pub_, velocity);
-  publishMessage(front_right_vel_pub_, velocity);
-  publishMessage(back_left_vel_pub_, velocity);
-  publishMessage(back_right_vel_pub_, velocity);
+	publishMessage(front_left_vel_pub_, velocity);
+	publishMessage(front_right_vel_pub_, velocity);
+	publishMessage(back_left_vel_pub_, velocity);
+	publishMessage(back_right_vel_pub_, velocity);
 }
 
+void followTrajectory(operations::TrajectoryWithVelocities* traj)
+{
+	std::cout << "Following custom trajectory message" << std::endl;
 
+	std::cout << "First x coordinate: " << traj->waypoints[0].pose.position.x << std::endl;
+  std::cout << "Frame of reference: " << traj->waypoints[0].header.frame_id << std::endl;
+}
+
+void followManually(float forward_velocity, float angular_velocity)
+{
+	float radius = 2.5, velocity;
+	
+	// If the forward velocity is 0, then the robot will rotate about its center,
+	// Hence the radius of rotation will be 0 and rotational velocity is
+	if(forward_velocity==0)
+	{
+		velocity = -angular_velocity;
+		radius = 0;
+	} 
+
+	// If the goal has forward velocity, then the robot should take radial turn
+	// Velocity will be the same as forward_velocity
+	else
+	{
+		velocity = forward_velocity;
+		radius = std::copysign(radius, angular_velocity);
+	}
+
+	// This will give us the steering angles and the velocities needed for taking the radial turn
+	std::vector<float> steering_angles;
+	std::vector<float> velocities;
+	steering_angles = NavigationAlgo::getSteeringAnglesRadialTurn(radius);
+	velocities = NavigationAlgo::getDrivingVelocitiessRadialTurn(radius, velocity);
+
+	// Steer and move the robot.
+	steerRobot(steering_angles);
+	moveRobotWheels(velocities);
+}
 
 /*******************************************************************/
 /****************** P U B L I S H E R   L O G I C ******************/
@@ -160,54 +201,54 @@ void moveRobotWheels(const float velocity)
  */
 void execute(const operations::NavigationGoalConstPtr& goal, Server* action_server)  
 {
-  // Velocities 
-  float forward_velocity, angular_velocity;
-  forward_velocity = goal->forward_velocity;
-  angular_velocity = goal->angular_velocity;
+	// Velocities 
+	float forward_velocity, angular_velocity;
+	forward_velocity = goal->forward_velocity;
+	angular_velocity = goal->angular_velocity;
+
+	std::cout << "Execute called" << std::endl;
 
 
-  // If the goal has sideways velocity, then the robots should take radial turn
-  if (angular_velocity != 0)
-  {
-    float radius = 2.5, velocity;
-    
-    // If the forward velocity is 0, then the robot will rotate about its center,
-    // Hence the radius of rotation will be 0 and rotational velocity is
-    if(forward_velocity==0)
-    {
-      velocity = -angular_velocity;
-      radius = 0;
-    } 
+	// If the goal has sideways velocity, then the robots should take radial turn
+	if (angular_velocity != 0)
+	{
+		followManually(forward_velocity, angular_velocity);
+	}
+	else
+	{
+		// Make service call to planner to get the trajectory
+		// TODO: Replace this temp code with real code
+		operations::TrajectoryWithVelocities* traj = new operations::TrajectoryWithVelocities();
 
-    // If the goal has forward velocity, then the robot should take radial turn
-    // Velocity will be the same as forward_velocity
-    else
-    {
-      velocity = forward_velocity;
-      radius = std::copysign(radius, angular_velocity);
-    }
+		geometry_msgs::PoseStamped* pose1 = new geometry_msgs::PoseStamped();
 
-    // This will give us the steering angles and the velocities needed for taking the radial turn
-    std::vector<float> steering_angles;
-    std::vector<float> velocities;
-    steering_angles = NavigationAlgo::getSteeringAnglesRadialTurn(radius);
-    velocities = NavigationAlgo::getDrivingVelocitiessRadialTurn(radius, velocity);
+		std_msgs::Header chassis;
+		chassis.frame_id = robot_name + "_small_chassis";
 
-    // Steer and move the robot.
-    steerRobot(steering_angles);
-    moveRobotWheels(velocities);
-  }
-  else
-  {
-    // Steer the robot straight, and execute the forward velocity
-    steerRobot(0);
-    moveRobotWheels(forward_velocity);
-  }
+    pose1->header = chassis;
+		pose1->pose.position.x = 2;
 
-  action_server->setSucceeded();
+		geometry_msgs::PoseStamped* pose2 = pose1;
+		pose2->pose.position.x = 4;
+
+		traj->waypoints.push_back(*pose1);
+		traj->waypoints.push_back(*pose2);
+
+		std_msgs::Float64 speed1;
+		std_msgs::Float64 speed2;
+
+		speed1.data = 0.5;
+		speed2.data = 0.7;
+
+		traj->velocities.push_back(speed1);
+		traj->velocities.push_back(speed2);
+
+		// Follow the trajectory generated by the local planner
+		followTrajectory(traj);
+	}
+
+	action_server->setSucceeded();
 }
-
-
 
 /*********************************************/
 /****************** M A I N ******************/
@@ -215,29 +256,29 @@ void execute(const operations::NavigationGoalConstPtr& goal, Server* action_serv
 
 int main(int argc, char** argv)
 {
-  // Check if the node is being run through roslauch, and have one parameter of RobotName_Number
-  if (argc != 4)
+	// Check if the node is being run through roslauch, and have one parameter of RobotName_Number
+	if (argc != 2)
   {
       // Displaying an error message for correct usage of the script, and returning error.
-      ROS_ERROR_STREAM("This Node must be launched via 'roslaunch' and needs an argument as <RobotName_Number>";);
+      ROS_ERROR_STREAM("Not enough arguments! Please pass in robot name with number.");
       return -1;
   }
-  else
-  {
-    std::string robot_name = (std::string)argv[1];
-    std::string node_name = robot_name + "_navigation_action_server";
+	else
+	{
+		robot_name = (std::string) argv[1];
+		std::string node_name = robot_name + "_navigation_action_server";
 
-    ros::init(argc, argv, node_name);
-    ros::NodeHandle nh;
+		ros::init(argc, argv, node_name);
+		ros::NodeHandle nh;
 
-    // Initialise the publishers for steering and wheel velocites
-    initPublishers(nh, robot_name);
-    
-    // Action server 
-    Server server(nh, NAVIGATION_ACTIONLIB, boost::bind(&execute, _1, &server), false);
-    server.start();
-    ros::spin();
+		// Initialise the publishers for steering and wheel velocites
+		initPublishers(nh, robot_name);
+		
+		// Action server 
+		Server server(nh, NAVIGATION_ACTIONLIB, boost::bind(&execute, _1, &server), false);
+		server.start();
+		ros::spin();
 
-    return 0;
-  }
+		return 0;
+	}
 }
