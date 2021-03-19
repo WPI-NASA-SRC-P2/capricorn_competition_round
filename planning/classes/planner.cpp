447996bff7ad0aff460862a8527c9df87d33ca96
FilipKernan <filip.kernan@gmail.com>
2021-03-19 17:46:25 -0400

    basic working cSpace function with basic testing

---
+++ planning/classes/planner.cpp
@@ -0,0 +1,228 @@
+// #include <ros/ros.h>
+
+#include "planner.h"
+
+
+Planner::Planner()
+{
+}
+
+Planner::~Planner()
+{
+}
+
+std::string Planner::getTeamName()
+{
+    // returns the private variable 
+    return team_name;
+}
+
+void Planner::setTeamName(const std::string& input_string)
+{
+    // team_name is assigned the value of Input string
+    // Doing this, you keep the private variable safe 
+    team_name = input_string;
+}
+
+// int grid_to_index(std::vector <Planner::cord> gridArray, Planner::cord cordinate) {
+//     for (int i = 0; gridArray.size(); i++) {
+//         if (gridArray.at(i) == cordinate) {
+//             return i;
+//         }
+//     }
+
+//     return -1;
+// }
+
+// Planner::cord index_to_grid(std::vector <Planner::cord> grid, int index){
+//     return grid.at(index);
+// }
+
+
+
+// std::vector <Planner::cord> eightNeighbors(std::vector <Planner::cord> grid, Planner::cord cord, int width){
+//     int cordIndex = grid_to_index(grid, cord);
+//     std::vector <Planner::cord> neighbors;
+//     neighbors.push_back(index_to_grid(grid, cordIndex + 1));
+//     neighbors.push_back(index_to_grid(grid, cordIndex - 1));
+//     neighbors.push_back(index_to_grid(grid, cordIndex - width + 1));
+//     neighbors.push_back(index_to_grid(grid, cordIndex - width));
+//     neighbors.push_back(index_to_grid(grid, cordIndex - width - 1));
+//     neighbors.push_back(index_to_grid(grid, cordIndex + width + 1));
+//     neighbors.push_back(index_to_grid(grid, cordIndex + width - 1));
+//     neighbors.push_back(index_to_grid(grid, cordIndex + width));
+//     return neighbors;
+// }
+
+std::vector <int> Planner::cSpace(std::vector <Planner::cord> grid, int width, std::vector <int> probibility, int threshold, int radius) 
+{
+    std::vector <int> newProbibility = probibility;
+    
+    for(int i = 0; i < probibility.size(); i++) {
+        
+        if (probibility.at(i) > threshold) 
+        {
+            std::vector <Planner::cord> origin;
+            origin.insert(origin.begin(), grid.at(i));
+            std::vector <Planner::cord> neighbors = findAllNeighbors(grid, width, origin, radius);
+            
+            int index = 0;
+            for(int j  = 0; j < neighbors.size(); j++) 
+            {
+                for (int i = 0; i < grid.size(); i++) 
+                {
+                    if (grid.at(i) == neighbors.at(j)) 
+                    {
+                        index = i;
+                    }
+                }
+                newProbibility.at(index) = 100; // there wasa a grid_to_index here
+            }
+        }
+        
+
+
+    }
+    return newProbibility;
+
+}
+
+std::vector <Planner::cord> Planner::findAllNeighbors(std::vector <Planner::cord> grid, int width, std::vector <Planner::cord> toSearch, int r)
+{
+    
+    std::vector <Planner::cord> toAdd;
+    for(int i = 0; i < toSearch.size(); i++)
+    {
+        std::vector <Planner::cord> neighbors;// =  eightNeighbors(grid, toSearch.at(i),width);
+        
+        // there was an eightNeighbors here
+        int cordIndex  = 0;
+        // there was a grid_to_index here
+        for (int j = 0; j < grid.size(); j++) 
+        {
+            // printf("%i, %i", grid.at(i).x, grid.at(i).y);
+            if (grid.at(j) == toSearch.at(i)) 
+            {
+                cordIndex = j;
+            }
+        }
+        // printf("finding Neighbors at (%i, %i), cordIndex of %i\n", toSearch.at(i).x, toSearch.at(i).y, cordIndex);
+        // changed index_to_grid to grid.at(cordIndex + ...)
+        //printf("adding %i, %i ,%i, %i ,%i, %i ,%i, %i \n",cordIndex + 1, cordIndex - 1, cordIndex - width + 1, cordIndex - width, cordIndex - width - 1, cordIndex + width + 1 , cordIndex + width - 1, cordIndex + width );
+        neighbors.push_back(grid.at(cordIndex + 1));
+        neighbors.push_back(grid.at(cordIndex - 1));
+        neighbors.push_back(grid.at(cordIndex - width + 1));
+        // printf("(%i, %i)\n", grid.at(cordIndex - width).x, grid.at(cordIndex - width).y);
+        neighbors.push_back(grid.at(cordIndex - width));
+        neighbors.push_back(grid.at(cordIndex - width - 1));
+        neighbors.push_back(grid.at(cordIndex + width + 1));
+        neighbors.push_back(grid.at(cordIndex + width - 1));
+        neighbors.push_back(grid.at(cordIndex + width)); 
+        
+
+        toAdd.insert(toAdd.end(), neighbors.begin(), neighbors.end());
+    }
+    // sort(toAdd.begin(),toAdd.end());
+    // toAdd.erase(std::unique(toAdd.begin(), toAdd.end()), toAdd.end());
+    
+
+    // sort(toAdd.begin(),toAdd.end());
+    // toAdd.erase(unique(toSearch.begin(), toSearch.end()), toAdd.end());
+    r--;
+    if (r > 0) 
+    {
+        
+        std::vector <Planner::cord> recusiveAdd = findAllNeighbors(grid, width, toAdd, r);
+
+        toAdd.insert(toAdd.end(), recusiveAdd.begin(), recusiveAdd.end());
+    }
+    return toAdd;
+  }
+
+
+/**
+ * @brief main function for quick-testing of the class
+ * 
+ */
+int main(int argc, char *argv[])
+{
+    // ROS initialization
+    // ros::init(argc, argv, "template_class_tester");
+    // ros::NodeHandle nh;
+
+    // // Creted an object of the class
+    // Planner template_class;
+
+    // // Setting the team name
+    // std::string temp_string = "Team Capricorn";
+    // template_class.setTeamName(temp_string);
+    
+    // // Getting the team name
+    // std::string team_name = template_class.getTeamName();
+    
+    // // Printing the team name
+    // ROS_INFO_STREAM("Team name from class object is: "<<team_name);
+
+
+    /*
+        5x5 gird
+        (3, 3) at 100
+        run cspace(r = 1, width = 5, threashold = 20)
+    */
+   std::vector <Planner::cord> grid;
+   std::vector <int> probibility;
+   for (int i = 1; i <= 5; i++) 
+   {
+    for (int j = 1; j <= 5; j++) 
+    {
+        Planner::cord currentCord;
+        currentCord.x = i;
+        currentCord.y = j;
+        grid.insert(grid.end(), currentCord);
+
+        if (i == 3 && j == 3) 
+        {
+            probibility.push_back(100);
+        } else 
+        {
+            probibility.push_back(0);
+        }
+
+    }
+    
+   }
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++) 
+        {
+            printf("(%i, %i)\t", grid.at((i *5) + j).x, grid.at((i*5) + j).y);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++) 
+        {
+            printf("(%i)\t", probibility.at((i * 5) + j));
+        }
+        printf("\n");
+    }
+    Planner* testPlanner = new Planner();
+    printf("\n");
+    std::vector<int> cSpaceOut = testPlanner->cSpace(grid, 5, probibility, 20, 1);
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++) 
+        {
+            printf("(%i)\t", cSpaceOut.at((i *5) + j));
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
---
+++ planning/classes/planner.h
@@ -0,0 +1,108 @@
+// header guards
+// https://www.educative.io/edpresso/what-are--sharpifndef-and--sharpdefine-used-for-in-cpp
+
+
+
+#ifndef PLANNER_CLASS_H
+#define PLANNER_CLASS_H
+
+// #include <ros/ros.h>
+#include <set>
+#include <algorithm>
+#include <tuple>
+#include <iostream>
+#include <vector>
+
+
+// Class names must be CamelCased and has a capital letter for each new word
+// http://wiki.ros.org/CppStyleGuide
+class Planner
+{
+
+/**
+ * @brief  Declare the private variables, functions and methods here.
+ *         Variables, functions and methods here cannot be accessed from outside the class
+ * 
+ */
+private:
+
+  // Private variable
+  // Variable names must be 
+  std::string team_name;
+
+
+/**
+ * @brief Declare the public variables, functions and methods here.
+ *        Things here can be accessed from outside the class.
+ * 
+ */
+public:
+  /**
+   * @brief Construct a new Planner Class object
+   * 
+   */
+  Planner();
+
+  /**
+   * @brief Destroy the Planner Class object
+   *        Pointers, threads etc used in the class must be delted in the destructor
+   * 
+   */
+  ~Planner();
+
+  struct cord 
+  {
+    int x;
+    int y;
+
+    cord& operator=(const cord& a)
+    {
+      x = a.x;
+      y = a.x;
+      return *this;
+    }
+
+    bool operator==(const cord& a) const 
+    {
+      return (x == a.x && y == a.y);
+    }
+
+    
+  };
+  
+
+  
+
+  std::vector<Planner::cord> findAllNeighbors(std::vector <Planner::cord> grid, int width, std::vector <Planner::cord> toSearch, int r);
+
+  int grid_to_index(std::vector <Planner::cord>, Planner::cord);
+  Planner::cord index_to_grid(std::vector <Planner::cord>, int);
+  std::vector <Planner::cord> eightNeighbors(std::vector <Planner::cord>, Planner::cord, int width);
+
+  // Functions and Methods should be camelCased with first letter lower-cased 
+  // Getters and Setters for interacting with the private variables/attributes
+  // https://www.w3schools.com/cpp/cpp_encapsulation.asp
+  
+  /**
+   * @brief Get the Team Name from the class object
+   * 
+   * @return std::string returns the team name
+   */
+  std::string getTeamName();
+
+  /**
+   * @brief Set the team name
+   * 
+   * @param string Input which will set the Team Name
+   *               Notice that the variable is passed by reference and is const.
+   *               https://www.learncpp.com/cpp-tutorial/passing-arguments-by-reference/
+   */
+  void setTeamName(const std::string& input_string);
+
+  std::vector <int> cSpace(std::vector <Planner::cord> grid, int width, std::vector <int> probibility, int threshold, int radius);
+
+
+
+};
+
+#endif // Planner_CLASS_H
