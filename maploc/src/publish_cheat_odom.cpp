c7dcd9309febf1c2f680109ca281717d025ed0e3
Ashay Aswale <asaswale@wpi.edu>
2021-03-23 22:48:05 -0400

    Navigation PR squash merge, teleop doesn't work

--- maploc/src/publish_cheat_odom.cpp
+++ maploc/src/publish_cheat_odom.cpp
@@ -13,7 +13,7 @@ NASA SPACE ROBOTICS CHALLENGE
 #include <tf/transform_broadcaster.h>
 #include <utils/common_names.h>
 
-#define UPDATE_HZ 10
+#define UPDATE_HZ 200
 
 /**
  * @brief This node is used to publish the ground truth position of the model given in the argument.
@@ -43,7 +43,7 @@ int main(int argc, char **argv)
     ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>(topic_name, 1, true);
 
     static tf::TransformBroadcaster br;
-    tf::Transform transform;
+    tf::StampedTransform transform;
 
     gazebo_msgs::GetModelState req;
     req.request.model_name = model_name;
@@ -64,6 +64,12 @@ int main(int argc, char **argv)
         transform.setOrigin(tf::Vector3(req.response.pose.position.x, req.response.pose.position.y, req.response.pose.position.z));
         tf::quaternionMsgToTF(req.response.pose.orientation, quat);
         transform.setRotation(quat);
+        transform.frame_id_ = COMMON_NAMES::MAP;
+        transform.child_frame_id_ = model_name + "_" + COMMON_NAMES::ROBOT_BASE;
+        transform.stamp_ = req.response.header.stamp;
+
+        br.sendTransform(transform);
+
         odom_msg.pose.pose = req.response.pose;
         odom_msg.twist.twist = req.response.twist;
         odom_msg.header = req.response.header;
--- operations/src/navigation_client.cpp
+++ operations/src/navigation_client.cpp
@@ -1,13 +1,21 @@
 #include <operations/NavigationAction.h> // Note: "Action" is appended
 #include <actionlib/client/simple_action_client.h>
 #include <utils/common_names.h>
+
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float64.h>
+
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 // typedef for the Action Server
 typedef actionlib::SimpleActionClient<operations::NavigationAction> Client;
 Client* client;
 
 using namespace COMMON_NAMES;
+
+std::string robot_name;
 
 /**
  * @brief Callback for the twist message of teleop message twist
@@ -19,34 +27,55 @@ void chatterCallback(const geometry_msgs::Twist::ConstPtr& twist)
     // Action message goal
     operations::NavigationGoal goal;
     
-    // Diverting values from twist to navigation
-    goal.forward_velocity = twist->linear.x * 5;
-    goal.angular_velocity = twist->angular.z;
-    
-    client->sendGoal(goal);
-    
-    // // Not really needed here. 
-    // // This can be better used as an actual feedback.
-    // client->waitForResult(ros::Duration(5.0));
-    // if (client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    //   printf("Yay! Robot should be moving\n");
-    // printf("Robot navigation state: %s\n", client->getState().toString().c_str());
+    //Simple waypoint 2 meters in front of the robot
+    geometry_msgs::PoseStamped t1;
+    t1.header.frame_id = robot_name + "_small_chassis";
+    t1.pose.position.x = 2.0;
+    t1.pose.position.y = 2.0;
+    t1.pose.position.z = 0;
 
+    t1.pose.orientation.w = 1;
+    t1.pose.orientation.x = 0;
+    t1.pose.orientation.y = 0;
+    t1.pose.orientation.z = 0;
+
+    //geometry_msgs::PoseStamped map_frame_waypoint = buffer.transform(t1, MAP);
+
+    goal.pose = t1;
+    goal.manual_driving = false;
+
+    printf("Sending auto goal to actionlib server\n");
+    client->sendGoal(goal);
+    ros::Duration(3).sleep();
+
+    // No manual driving
+    goal.manual_driving = true;
+    goal.forward_velocity = 0;
+    goal.angular_velocity = -1;
+    printf("Sending manual goal to actionlib server\n");
+    client->sendGoal(goal);
+    ros::Duration(3).sleep();
+
+    printf("Re-sending auto goal to actionlib server\n");
+    goal.manual_driving = false;
+    goal.forward_velocity = 0;
+    goal.angular_velocity = 0;
+    client->sendGoal(goal);
 }
 
 int main(int argc, char** argv)
 {
-  // Check if the node is being run through roslauch, and have one parameter of RobotName_Number
-  if (argc != 4)
+  // Ensure the robot name is passed in
+  if (argc != 2 && argc != 4)
   {
       // Displaying an error message for correct usage of the script, and returning error.
-      ROS_ERROR_STREAM("This Node must be launched via 'roslaunch' and needs an argument as <RobotName_Number>";);
+      ROS_ERROR_STREAM("Not enough arguments! Please pass in robot name with number.");
       return -1;
   }
   else
   {
     // Robot Name from argument
-    std::string robot_name(argv[1]);
+    robot_name = argv[1];
     std::string node_name = robot_name + "_navigation_action_client";
     ros::init(argc, argv, node_name);
     ros::NodeHandle nh;
@@ -54,9 +83,13 @@ int main(int argc, char** argv)
     // Subscribing to teleop topic
     ros::Subscriber sub = nh.subscribe("/cmd_vel", 1000, chatterCallback);
 
+    printf("Nav client: Instantiating client instance\n");
+
     // initialize client
     client = new Client(NAVIGATION_ACTIONLIB, true);
+    printf("Waiting for server...\n");
     client->waitForServer();
+    printf("Done waiting. Spinning\n");
 
     ros::spin();
     return 0;
