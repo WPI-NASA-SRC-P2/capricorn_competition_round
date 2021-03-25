c82da827f0272ebab5ebbc8b9ea2d6715f80c5c0
MahimanaGIT <mahimnabhatt@gmail.com>
2021-03-24 23:27:42 -0400

    Updating navigation vision cpp

--- operations/src/navigation_vision.cpp
+++ operations/src/navigation_vision.cpp
@@ -1,113 +1,36 @@
-#include <ros/ros.h>
+/*
+Author: Mahimana Bhatt
+Email: mbhatt@wpi.edu
+TEAM CAPRICORN
+NASA SPACE ROBOTICS CHALLENGE
+*/
+
+#include <perception/Find_PP_RSAction.h>
+#include <actionlib/client/simple_action_client.h>
 #include <perception/ObjectArray.h>
 #include <perception/Object.h>
-#include <operations/NavigationAction.h> // Note: "Action" is appended
-#include <actionlib/client/simple_action_client.h>
 #include <utils/common_names.h>
-
-#include <geometry_msgs/Twist.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <std_msgs/Float64.h>
-
-#include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-// typedef for the Action Server
-typedef actionlib::SimpleActionClient<operations::NavigationAction> Client;
-Client* client;
-
-float height = 480;
-float width = 640;
 
 std::string robot_name;
 
-#define UPDATE_HZ 20
+void bb_and_objects_callback(const perception::ObjectArray& objects) 
+{
 
-void objects_callback(const perception::ObjectArray& objects, std::string desired_object_label) 
-{  
-    
-    bool obj_detected;
-    
-    // Create a goal object and turn on manual driving
-    operations::NavigationGoal goal;
-    goal.manual_driving = true;
-
-    // Initialize location and size variables
-    float center_obj = -1;
-    float height_obj = -1;
-        
-    // Find the desired object
-    for(int i = 0; i < objects.number_of_objects; i++) 
-    {   
-        if(objects.obj[i].label == desired_object_label) {
-            // Store the object's center and height
-            center_obj = objects.obj[i].center.x;
-            height_obj = objects.obj[i].size_y;
-        }
-    }
-        
-    // Initialize error, P Control, and necessary thresholds 
-    int proportional_angle = 15;
-    float error_angle;
-    int error_angle_threshold = 7;
-        
-    int proportional_height = 15;
-    float error_height;
-    int height_threshold = 400;
-    int error_height_threshold = 7;
-
-    if(center_obj == -1)
-    {
-        obj_detected = false;
-        return;
-    }
-    else
-    {
-        obj_detected = true;
-        error_angle = width - center_obj;
-        error_height = height_threshold - height_obj;
-    }
-
-    if(error_angle < error_angle_threshold)
-    {
-        // If object is centered, stop turning.
-        goal.angular_velocity = 0;
-
-        if(error_height < error_height_threshold)
-        {
-            // If the object is big enough, stop the robot
-            goal.forward_velocity = 0;
-        }
-        else
-        {
-            // Keep driving forward
-            goal.forward_velocity = error_height * proportional_height;
-        }
-
-    }
-    else
-    {
-        // If object is not centered, turn.
-        goal.angular_velocity = error_angle / width * 3.14 / proportional_angle;
-        goal.forward_velocity = 0;
-    }
-    
-    client->sendGoal(goal);
+    ros::shutdown();
 }
 
 int main(int argc, char** argv)
 {
     std::string name(argv[1]);
     robot_name = name;
+    ros::init(argc, argv, robot_name + COMMON_NAMES::PR_DATASET_NODE_NAME);
 
-    ros::init(argc, argv, robot_name + COMMON_NAMES::NAVIGATION_VISION_NODE_NAME);
     ros::NodeHandle nh;
+    ros::AsyncSpinner spin(1);
     
-    ros::Subscriber objects_sub = nh.subscribe(COMMON_NAMES::CAPRICORN_TOPIC + robot_name + COMMON_NAMES::OBJECT_DETECTION_OBJECTS_TOPIC, 1, &objects_callback);
-  
-    client = new Client(NAVIGATION_ACTIONLIB, true);
-    client->waitForServer();
+    ros::Subscriber objects_sub = nh.subscribe(COMMON_NAMES::CAPRICORN_TOPIC + robot_name + COMMON_NAMES::OBJECT_DETECTION_OBJECTS_TOPIC, 1, &bb_and_objects_callback);
 
-    ros::spin();
+    ros::spin(); 
+    
     return 0;
 }
