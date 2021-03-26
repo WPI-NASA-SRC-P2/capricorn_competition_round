cae20e4d57ec5d61133d87f83a58d3d737b0061f
chrisdemaio <cjdemaio@wpi.edu>
2021-03-25 20:15:48 -0400

    debug--it works!

--- operations/src/navigation_vision.cpp
+++ operations/src/navigation_vision.cpp
@@ -1,3 +1,11 @@
+/* TODO:
+- Tune the PID controller for turning
+- Investigate rightward jerking at beginning of turn
+- Convert into action library
+  - Input: Label
+  - Output: Progress, Done? 
+*/
+
 #include <operations/NavigationAction.h> // Note: "Action" is appended
 #include <actionlib/client/simple_action_client.h>
 #include <utils/common_names.h>
@@ -18,6 +26,10 @@ Client* client;
 using namespace COMMON_NAMES;
 
 std::string robot_name;
+float sum_error_angle = 0.0;
+float prev_error_angle = 0.0;
+float sum_error_height = 0.0;
+float prev_error_height = 0.0;
 
 void objects_callback(const perception::ObjectArray& objects) 
 {
@@ -32,7 +44,7 @@ void objects_callback(const perception::ObjectArray& objects)
   // Initialize location and size variables
   float center_obj = -1;
   float height_obj = -1;
-  float width = -1;
+  float width = 640.0;
 
   // Find the desired object
   for(int i = 0; i < objects.number_of_objects; i++) 
@@ -41,85 +53,71 @@ void objects_callback(const perception::ObjectArray& objects)
       // Store the object's center and height
       center_obj = objects.obj[i].center.x;
       height_obj = objects.obj[i].size_y;
-      width = objects.obj[i].size_x;
     }
   }
 
   // Initialize error, P Control, and necessary thresholds 
-  int proportional_angle = 15;
+  float proportional_angle = 0.001;
+  float integral_angle = 0.0000001;
+  float derivative_angle = 0.001;
   float error_angle;
-  int error_angle_threshold = 3;
+  int error_angle_threshold = 40;
 
-  int proportional_height = 15;
+  float proportional_height = 0.02;
+  float integral_height = 0.0;
+  float derivative_height = 0.0;
   float error_height;
   int height_threshold = 300;
-  int error_height_threshold = 3;
+  int error_height_threshold = 10;
 
   if(center_obj == -1)
   {
     obj_detected = false;
-    goal.angular_velocity = 2;
+    goal.angular_velocity = 0.3;
     goal.forward_velocity = 0;
-    return;
   }
   else
   {
     obj_detected = true;
-    error_angle = width - center_obj;
+    error_angle = (width / 2.0) - center_obj;
     error_height = height_threshold - height_obj;
-  }
+  
 
-  if(error_angle < error_angle_threshold)
-  {
-    // If object is centered, stop turning.
-    goal.angular_velocity = 0;
-
-    if(error_height < error_height_threshold)
+    if(abs(error_angle) < error_angle_threshold)
     {
-        // If the object is big enough, stop the robot
-        goal.forward_velocity = 0;
-        ros::shutdown();
+      // If object is centered, stop turning.
+      goal.angular_velocity = 0;
+
+      if(error_height < error_height_threshold)
+      {
+          // If the object is big enough, stop the robot
+          goal.forward_velocity = 0;
+          client->sendGoal(goal);
+          ros::shutdown();
+      }
+      else
+      {
+          // Keep driving forward
+          sum_error_height += error_height;
+          goal.forward_velocity = error_height * proportional_height + sum_error_height * integral_height + (error_height - prev_error_height) * derivative_height;
+      }
     }
     else
     {
-        // Keep driving forward
-        goal.forward_velocity = error_height * proportional_height;
+      // If object is not centered, turn.
+      sum_error_angle += error_angle;
+      goal.angular_velocity = error_angle * proportional_angle + sum_error_angle * integral_angle + (error_angle - prev_error_angle) * derivative_angle;
+      goal.forward_velocity = 0;
     }
   }
-  else
-  {
-    // If object is not centered, turn.
-    goal.angular_velocity = error_angle / width * 3.14 / proportional_angle;
-    goal.forward_velocity = 0;
-  }
+
+  prev_error_angle = error_angle;
+  prev_error_height = error_height;
 
   ROS_INFO_STREAM("ERROR Height: "<<error_height<<", Angle: "<<error_angle);
+  ROS_INFO_STREAM("Angular velocity: "<<goal.angular_velocity);
   client->sendGoal(goal);
-  ros::Duration(0.1).sleep();
 
-
-
-  // operations::NavigationGoal goal;
-  
-  // goal.drive_mode = NAV_TYPE::MANUAL;
-  // goal.forward_velocity = 2;
-  // client->sendGoal(goal);
-  // ros::Duration(2).sleep();  
-
-
-  // goal.drive_mode = NAV_TYPE::MANUAL;
-  // goal.angular_velocity = 2;
-  // client->sendGoal(goal);
-
-
-  // ros::Duration(2).sleep();
-
-
-  // goal.drive_mode = NAV_TYPE::MANUAL;
-  // goal.forward_velocity = 0;
-  // goal.angular_velocity = 0;
-  // client->sendGoal(goal);
-  // ros::Duration(10).sleep();
 }
 
 int main(int argc, char** argv)
