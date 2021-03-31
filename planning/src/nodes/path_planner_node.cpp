c28e7f2f31b18911abf60760edf31ff9de9d402b
[Rushab Patil] <[rdpatil1@outlook.com]>
2021-03-30 23:15:13 -0400

    ROS Service Server

--- planning/src/nodes/path_planner_node.cpp
+++ planning/src/nodes/path_planner_node.cpp
@@ -1,21 +1,17 @@
 #include <ros/ros.h>
-
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/Point.h>
-
-
 #include <astar.h>
 #include <cspace.h>
 
 //How fast the main loop of the node will run.
 #define UPDATE_HZ 10
-
 ros::Publisher publisher;
-
 using geometry_msgs::Point;
 
-void callback(const nav_msgs::OccupancyGrid oGrid) {
+void callback(const nav_msgs::OccupancyGrid oGrid)
+{
     Point origin;
     origin.x = 180;
     origin.y = 0;
@@ -23,26 +19,29 @@ void callback(const nav_msgs::OccupancyGrid oGrid) {
     Point target;
     target.x = 180;
     target.y = 300;
-
     auto CSpace = CSpace::GetCSpace(oGrid, 50, 1);
     auto path = AStar::FindPathOccGrid(CSpace, target, origin);
-
     publisher.publish(path);
+}
+
+bool trajectoryGeneration(planning::trajectory::Request &req, planning::trajectory::Response &res)
+{
+    res.trajectory = trajectoryGenerator(req.targetPose);
+    return true;
 }
 
 int main(int argc, char *argv[])
 {
     // ROS initialization
     ros::init(argc, argv, "path_planner");
-
     ros::NodeHandle nh;
-
     publisher = nh.advertise<nav_msgs::Path>("/capricorn/navigation_path", 1000);
-
-    //Subscribe to the node publishing map values 
-    ros::Subscriber indexValues = nh.subscribe("/capricorn/Ground_Truth_Map", 1000, callback);
     
+    //Subscribe to the node publishing map values
+    ros::Subscriber indexValues = nh.subscribe("/capricorn/Ground_Truth_Map", 1000, callback);
+   
+    //creates a service
+    ros::ServiceServer service = n.advertiseService("trajectoryGenerator", trajectoryGenerator);
     ros::spin();
-
     return 0;
 }
