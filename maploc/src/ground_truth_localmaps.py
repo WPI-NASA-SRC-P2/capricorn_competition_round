84278e11beccc00d745ae878d082e8e970fa1b20
Albert Enyedy <aenyedy56@gmail.com>
2021-04-23 21:47:58 -0400

    updated ground_truth_localmaps.py to reflect the new blank maps method we're using

--- maploc/src/ground_truth_localmaps.py
+++ maploc/src/ground_truth_localmaps.py
@@ -7,68 +7,99 @@ import numpy as np
 from gazebo_msgs.srv import GetModelState
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import MapMetaData
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseWithCovariance
 from math import pi
 import sys
+
+from perception.msg import Object
+from perception.msg import ObjectArray
+
+from tf import TransformListener
 
 # tf2 reqs
 #import tf_conversions
 #import tf2_ros
 
-# Acquiring object Poses
-class Block:
-    def __init__(self, name, relative_entity_name):
-        self._name = name
-        self._relative_entity_name = relative_entity_name
+# TODO: 
+# - take object detection instead of gazebo
+# - no rtabmap, just make local map
+# - make sure that the file works for not just small scout 1 (change namespace)
 
-class Models_state:
+# Object Plotter Class will store the object list received from subscriber
+#  and will update it within itself 
+class Object_Plotter:
 
-    _blockListDict = {
-        'small_scout_1': Block('small_scout_1', 'link'),
-        'small_excavator_1': Block('small_excavator_1', 'link'),
-        'small_hauler_1':Block('small_hauler_1', 'link'),
-        'repair_station': Block('repair_station', 'link'),
-        'processing_plant': Block('processing_plant', 'link')
-    }
+    def __init__(self):
+        # blank 20x20 occupancy grid fields
+        # pose subscriber updates robot pose as it moves
+        self.robot_pos_sub = self.object_sub = rospy.Subscriber("/capricorn/small_scout_1/camera/odom", Odometry, self.robot_cb)
+        self.robot_pose = PoseWithCovariance()# value indicates the robot base frame pose
+        self.occ_grid = OccupancyGrid()
 
-    def show_gazebo_models(self):
-        All_coordinate = []
-        try:
-            model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-            for block in self._blockListDict.values():
-                blockName = str(block._name)
-                resp_coordinates = model_coordinates(blockName, block._relative_entity_name)
-                x = resp_coordinates.pose.position.x
-                y = resp_coordinates.pose.position.y
-                z = resp_coordinates.pose.position.z
-                All_coordinate.append((x, y, z))
-
-        except rospy.ServiceException as e:
-            rospy.loginfo("Get Model State service call failed:  {0}".format(e))
-
-        return All_coordinate
-
-    # set which of the five entities is the origin entity for the local map
-    # i.e., input arg 'small_scout_1' would result in obtaining poses w.r.t. small_scout_1
-    def setRelativeEntity(self, entityname):
-        self._blockListDict = {
-            'small_scout_1': Block('small_scout_1', entityname),
-            'small_excavator_1': Block('small_excavator_1', entityname),
-            'small_hauler_1':Block('small_hauler_1', entityname),
-            'repair_station': Block('repair_station', entityname),
-            'processing_plant': Block('processing_plant', entityname)
-        }
+        # object detection data and subscriber
+        self.obj_list = ObjectArray()
+        self.object_sub = rospy.Subscriber("/capricorn/small_scout_1/object_detection/objects", ObjectArray, self.object_cb)
+        
+        # occupancy grid publisher
+        self.occGridPub = rospy.Publisher("/capricorn/object_detection_map", OccupancyGrid, queue_size=1)
     
-    # obtain the x,y,z position w.r.t. base frame of desired entity
-    def getXYCoordinates(self, entityname):
-        xyBlock = self._blockListDict[entityname]
-        model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-        resp_coordinates = model_coordinates(entityname, xyBlock._relative_entity_name)
-        x = resp_coordinates.pose.position.x
-        y = resp_coordinates.pose.position.y
-        z = resp_coordinates.pose.position.z
-        return [x,y,z]
+    # NECESSARY FUNCTIONS
 
+    # subscriber callback to robot pose, updates robot pose as it moves
+    def robot_cb(self, odom):
+        self.robot_pose = odom.pose
+
+    # subscriber callback to object detection, updates detected obstacle list
+    def object_cb(self, objlist):
+        self.obj_list = objlist
+
+    # initialize/refresh the blank 20x20 map centered on the robot 
+    def init_occ_grid(self):
+        metadata = MapMetaData()
+        # define dimensions of blank occupancy grid
+        metadata.resolution = 0.05 # (m/pixel) 0.05 matches rtabmap resolution
+        metadata.width = 20/metadata.resolution # sets the map to be 20m x 20m regardless of resolution
+        metadata.height = 20/metadata.resolution 
+
+        # define origin of blank occupancy grid (should correspond to robot pose)
+        base_frame = self.robot_pose.pose
+        metadata.origin = base_frame
+
+        # set up the 2D OccupancyGrid with robot base frame as origin
+        # - the origin is at bottom left. The origin will be translated to the center of the map in plotting function and publishing
+        self.occ_grid.info = metadata
+
+        # initialize map data as all zeros
+        self.occ_grid.data = [0] * (self.occ_grid.metadata.width*self.occ_grid.metadata.height)
+
+    # transform the object list from camera frame to base frame
+    def transform(self, old_x, old_y, robotname):
+        tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(tfBuffer)
+
+
+        try:
+            trans = tfBuffer.lookup_transform(robotname+"_left_camera_optical", robotname+'base_footprint', rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
+  
+
+        new_x = trans.transform.translation.x + old_x 
+        new_y = trans.transform.translation.y + old_y
+    
+    # plot single object
+    # - basically modify the add_obstacle method
+
+    # plot all objects in object list
+    # - basically plot single object many times (across length of object list)
+
+    # publish the updated occupancy grid
+    # - publish self.occ_grid after finished
+
+#____________________________________________________________________________
+# OLD CODE SNIPPETS BELOW: TODO: modify them to integrate with new class
 
 # 100 = obstacle, -1 = unexplored, 0 = free
 def add_obstacle(occGrid, obx, oby, radius):
@@ -218,7 +249,6 @@ if __name__=="__main__":
     obstacleList = initializeObstacleList(baseFrameEntityName)
 
     # set up occupancy grid publisher
-    occGridPub = rospy.Publisher("/capricorn/Ground_Truth_Map", OccupancyGrid, queue_size=1)
     
     # loop to keep updating the map
     while not rospy.is_shutdown():
