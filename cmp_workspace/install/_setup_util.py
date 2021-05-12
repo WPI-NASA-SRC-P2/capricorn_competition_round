db94425f861238894c325ad2047983664a80c1c4
srcp2 <5213392-scheducation@users.noreply.gitlab.com>
2021-05-12 16:26:16 -0500

    making release commit for SRCP2: Wed May 12 16:26:16 CDT 2021

--- cmp_workspace/install/_setup_util.py
+++ cmp_workspace/install/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/srcp2/cmp_workspace/install;/home/srcp2/ros_workspace/install;/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/srcp2/ros_workspace/install;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
