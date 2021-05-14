09cf987764a84422da7262cd0dc837e1b204d878
Albert Enyedy <aenyedy56@gmail.com>
2021-05-14 18:09:14 -0400

    cleaned up documentation on reset_odom.cpp node

--- maploc/src/reset_odom.cpp
+++ maploc/src/reset_odom.cpp
@@ -33,37 +33,19 @@ NASA SPACE ROBOTICS CHALLENGE
     - end result: robot's odometry is reset w.r.t. the input pose
 */
 
-// bool gt_first_hauler_1 = true;
-// bool gt_first_excavator_1 = true;
-// bool gt_first_scout_1 = true;
-
-// stored ground truths
-geometry_msgs::PoseStamped gt_pose_hauler_1;
+// stored ground truths for each robot
 std::map<std::string, geometry_msgs::PoseStamped> gt_pose_values;
 
-
-//Startup ROS
-
-// robot_first["gt_first_hauler_1"] = true;
-// robot_first["gt_first_excavator_1"] = true;
-// robot_first["gt_first_scout_1"] = true;
-
-// stored whether first ground truth call made
+// stored whether first ground truth call made for each robot
 std::map<std::string, bool> robot_first;
 
-/**
- * @brief:
- * DISCLAIMER: 
- * 
- * @param argv : REQUIRED paramter is the robot name eg. small_scout_1
- * @return int 
- */
-
-// obtains the ground truth pose of the rover using the 1x per simulation ground truth service call
+// obtains the ground truth pose of the rover using the one time per simulation ground truth service call
 geometry_msgs::PoseStamped getTruePose(std::string robot_name){
     ros::NodeHandle nh;
+    
     // service client for calling the LocalizationSrv service for each rover
     ros::ServiceClient get_true_pose_client;
+    
     // set up the ground truth service call client
     get_true_pose_client = nh.serviceClient<srcp2_msgs::LocalizationSrv>("/" + robot_name + COMMON_NAMES::TRUE_POSE_SRV);
     
@@ -74,6 +56,7 @@ geometry_msgs::PoseStamped getTruePose(std::string robot_name){
     // variable for storing the service pose
     geometry_msgs::Pose pose_wrt_heightmap;
 
+    // obtain the true pose of the rover using the service if possible
     if(get_true_pose_client.call(loc_pose)){
         pose_wrt_heightmap = loc_pose.response.pose;
         ROS_INFO("True Pose Obtained");
@@ -82,6 +65,7 @@ geometry_msgs::PoseStamped getTruePose(std::string robot_name){
         ROS_INFO("True Pose Not Obtained, we'll get em next time");
         // return an empty pose (TODO: proper exception thrown)
     }        
+
     // convert true pose into a PoseStamped
     geometry_msgs::PoseStamped stamped_pose;
     stamped_pose.pose = pose_wrt_heightmap;
@@ -89,12 +73,12 @@ geometry_msgs::PoseStamped getTruePose(std::string robot_name){
     // return PoseStamped true pose (or empty pose if true pose not obtained)
     return stamped_pose;  
 }
-// irst robot 
-// resets the rtabmap odometry based on the input PoseStampedgeometry_msgs::PoseStamped
+
+// resets the rtabmap odometry based on the input PoseStamped
 bool resetOdomPose(std::string robot_name, geometry_msgs::PoseStamped stamped_pose) {
-    ROS_INFO("stamped pose = ");
-    ROS_INFO_STREAM(stamped_pose);
     ros::NodeHandle nh;
+    
+    // convert the poseStamped's quaternion into roll,pitch,yaw for the rtabmap reset odom service call
     tf2::Quaternion q(stamped_pose.pose.orientation.x,
                         stamped_pose.pose.orientation.y,
                         stamped_pose.pose.orientation.z,
@@ -121,8 +105,9 @@ bool resetOdomPose(std::string robot_name, geometry_msgs::PoseStamped stamped_po
 
     ROS_INFO("Rtabmap client loaded");
 
-    ROS_INFO("POSE = ");
-    ROS_INFO_STREAM(pose.request);
+    // debugging info to double check if the pose is properly being converted
+    // ROS_INFO("POSE = ");
+    // ROS_INFO_STREAM(pose.request);
 
     // reset the odometry w.r.t. the input pose
     if(rtabmap_client.call(pose)){
@@ -138,35 +123,30 @@ bool resetOdomPose(std::string robot_name, geometry_msgs::PoseStamped stamped_po
 
 // service callback function that resets the odometry of the specified rover w.r.t. the specified pose
 // if not_gt
-// method returns a boolean due to services needing to have a boolean return statement
-//bool resetOdom(geometry_msgs::PoseStamped &ref_pose, std::string &target_robot_name, bool& not_gt) {
-//bool resetOdom()
+// method returns a boolean due to services needing to have a boolean return statement and to provide feedback on success of service call
 bool resetOdom(maploc::ResetOdom::Request &req, maploc::ResetOdom::Response &res) {
-    ROS_INFO("req = ");
-    ROS_INFO_STREAM(req);
-    /*
-    geometry_msgs/PoseStamped ref_pose
-    string target_robot_name
-    bool not_gt
-    ---
-    bool success
-    */
+
+    // obtain the service call request data
     geometry_msgs::PoseStamped ref_pose = req.ref_pose;
     std::string target_robot_name = req.target_robot_name;
     bool not_gt = req.not_gt;
+    
+    // if using ground truth, get true pose if first call, else use the stored in memory ground truth pose
     if(!not_gt) {
-        // map<string, bool>::iterator it;
         // calls the desired robot within the robot_first map
         std::map<std::string, bool>::iterator it = robot_first.find(target_robot_name);
+
         // if the robot called does not exist, throw an exception
         // TODO: make sure this throws an exception 
         if (it == robot_first.end()) {
             ROS_INFO("No robot was found with input target robot name!");
+            // response for service returns false
             res.success = false;
             return false;
         }
         // if gt_first_robot call is true
         if (it->second) {
+            // obtain the ground truth pose of the rover using the provided service call
             ref_pose = getTruePose(target_robot_name);
             // store the ground truth pose of the rover for subsequent calls
             gt_pose_values[target_robot_name] = ref_pose;
@@ -180,8 +160,9 @@ bool resetOdom(maploc::ResetOdom::Request &req, maploc::ResetOdom::Response &res
         resetOdomPose(target_robot_name, ref_pose);
         res.success = true;
         return true;
+
     } else {
-        // resets the rtabmap odom based on the specified pose
+        // resets the rtabmap odom based on the specified pose from the service call (i.e., the hauler's pose if service called by excavator)
         resetOdomPose(target_robot_name, ref_pose);
         res.success = true;
         return true;
@@ -194,27 +175,16 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "odom_reset_service_node");
     ros::NodeHandle nh;
 
-
-    // robot_first["gt_first_hauler_1"] = true;
-    // robot_first["gt_first_excavator_1"] = true;
-    // robot_first["gt_first_scout_1"] = true;
-
+    // ensure that the memory of which rovers already used their ground truth call is reset to true upon starting the node
     robot_first["small_hauler_1"] = true;
     robot_first["small_scout_1"] = true;
     robot_first["small_excavator_1"] = true;
 
-    // gt_first_hauler_1 = true;
-    // gt_first_excavator_1 = true;
-    // gt_first_scout_1 = true;
-
-    // gt_pose_hauler_1 = 
-    // gt_pose_values
-    // set up service for obtaining rover name and resets pose
+    // set up service for reseting the odometry of the rover 
     ros::ServiceServer service = nh.advertiseService("/capricorn/reset_rover_odom_srv", resetOdom);
-    // set up reset for odometry
 
-    // return an indicator that reset is complete
-    ROS_INFO("odometry has been reset");
+    // indicate that reset is complete
+    ROS_INFO("reset rover odometry service, online");
 
     ros::spin();
     return 0;
