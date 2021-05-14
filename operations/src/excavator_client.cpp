ee1041bead2e0d47fa259242a64b9d15f03e2df6
MahimanaGIT <mahimnabhatt@gmail.com>
2021-05-14 14:34:30 -0400

    Excavator state machine changes, only for testing

--- operations/src/excavator_client.cpp
+++ operations/src/excavator_client.cpp
@@ -21,36 +21,38 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "excavator_client");
   Client client(EXCAVATOR_ACTIONLIB, true);
   client.waitForServer();
+  ROS_INFO("FOUND SERVER");
 
-  operations::ExcavatorGoal goal;
-  goal.task = START_DIGGING; // START_DIGGING = 1
-  goal.target.x = 0.7; // set of target digging values to the left of the excavator
-  goal.target.y = 2;
-  goal.target.z = 0;
-  client.sendGoal(goal);
+  while(true && ros::ok())
+  {
+    operations::ExcavatorGoal goal;
+    goal.task = START_DIGGING; // START_DIGGING = 1
+    goal.target.x = 0.7; // set of target digging values to the left of the excavator
+    goal.target.y = 2;
+    goal.target.z = 0;
+    client.sendGoal(goal);
 
-  std::string message1(client.getState().toString().c_str());
-  ROS_INFO_STREAM("Current State: " + message1);
-  client.waitForResult(ros::Duration(SLEEP_DURATION));
-  if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO_STREAM("Yay! The scoop is now full");
+    std::string message1(client.getState().toString().c_str());
+    ROS_INFO_STREAM("Current State: " + message1);
+    client.waitForResult();
+    if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO_STREAM("Yay! The scoop is now full");
 
-  ros::Duration(SLEEP_DURATION).sleep(); // Delay between digging and unloading tasks
+    goal.task = START_UNLOADING; // START_UNLOADING = 2
+    goal.target.x = 0.7; // set of target dumping values to the right of the excavator
+    goal.target.y = -2;
+    goal.target.z = 0;
+    client.sendGoal(goal);
+    
+    std::string message2(client.getState().toString().c_str());
+    ROS_INFO_STREAM("Current State: " + message2);
+    client.waitForResult();
+    if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO_STREAM("Yay! The scoop is now empty");
 
-  goal.task = START_UNLOADING; // START_UNLOADING = 2
-  goal.target.x = 0.7; // set of target dumping values to the right of the excavator
-  goal.target.y = -2;
-  goal.target.z = 0;
-  client.sendGoal(goal);
-  
-  std::string message2(client.getState().toString().c_str());
-  ROS_INFO_STREAM("Current State: " + message2);
-  client.waitForResult(ros::Duration(SLEEP_DURATION));
-  if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO_STREAM("Yay! The scoop is now empty");
-
-  std::string message3(client.getState().toString().c_str());
-  ROS_INFO_STREAM("Current State: " + message3);
+    std::string message3(client.getState().toString().c_str());
+    ROS_INFO_STREAM("Current State: " + message3);
+  }
 
   return 0;
 }
