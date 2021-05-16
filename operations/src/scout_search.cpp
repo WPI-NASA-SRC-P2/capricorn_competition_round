4c3b9dca170419b9743a65b9053b4f0e57625a61
Ashay Aswale <asaswale@wpi.edu>
2021-05-15 23:48:21 -0400

    spiral with obstacle avoidance

--- operations/src/scout_search.cpp
+++ operations/src/scout_search.cpp
@@ -14,6 +14,11 @@ Command Line Arguments Required:
 #include <actionlib/server/simple_action_server.h>
 #include <operations/obstacle_avoidance.h>
 
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <nav_msgs/Odometry.h>
+#include <operations/navigation_algorithm.h>
+
 #define UPDATE_HZ 10
 
 typedef actionlib::SimpleActionClient<operations::NavigationAction> Client;
@@ -27,6 +32,73 @@ const float INIT_VALUE = -100.00, FORWARD_VELOCITY = 0.8;
 std::mutex g_objects_mutex;
 int g_lost_detection_times = 0, g_true_detection_times = 0, g_revolve_direction = -1;
 
+geometry_msgs::PoseStamped g_robot_pose;
+bool cb_init = false; // Used to check if the callback has been initiated yet or not
+static std::vector<geometry_msgs::PointStamped> g_spiral_points;
+
+double g_last_dist = 0.0;
+bool g_going_to_goal = false;
+bool g_new_trajectory = false;
+const double CHECKPOINT_THRESHOLD = 2.0;
+
+/**
+ * @brief Callback to the robot pose topic
+ * 
+ * @param msg 
+ */
+void updateRobotPose(const nav_msgs::Odometry::ConstPtr& msg)
+{
+	g_robot_pose.header = msg->header;
+	g_robot_pose.pose = msg->pose.pose;
+  cb_init = true;
+}
+
+
+/**
+ * @brief Get the Orientation object
+ *        The tangential direction of a point is 
+ *        approximated by the segment joining the previous and the next point
+ * 
+ * @param p0  Point previous to the current point of interest
+ * @param p2  Point next to the current point of interest
+ * @return geometry_msgs::Quaternion orientation tangential to the spiral
+ */
+geometry_msgs::Quaternion getOrientation(const geometry_msgs::Point& p0, const geometry_msgs::Point& p2)
+{
+  float d_x = p2.x - p0.x;
+  float d_y = p2.y - p0.y;
+
+  double yaw = atan2(d_y, d_x);
+  tf2::Quaternion myQuaternion;
+  myQuaternion.setRPY( 0, 0, yaw);
+
+  geometry_msgs::Quaternion quat_msg;
+  tf2::convert(myQuaternion, quat_msg);
+  return quat_msg;
+}
+
+
+/**
+ * @brief Get the Center Of Rotation object
+ *        Returns the center of the circle formed by the first three points in spiral
+ * 
+ * @param spiral_points   Points forming the spiral curvature
+ * @return geometry_msgs::PointStamped  Center of the circle of first three points
+ */
+geometry_msgs::PointStamped getCenterOfRotation(const std::vector<geometry_msgs::PointStamped>& spiral_points)
+{
+  std::vector<geometry_msgs::PointStamped> temp_points;
+  temp_points.push_back(spiral_points.at(0));
+  temp_points.push_back(spiral_points.at(1));
+  temp_points.push_back(spiral_points.at(2));
+  
+  double radius = NavigationAlgo::getRadiusOfThreePointsCircle(temp_points);
+  geometry_msgs::PointStamped center_of_rot;
+  center_of_rot.point.y = radius;
+  return center_of_rot;
+}
+
+
 /**
  * @brief Callback function which subscriber to Objects message published from object detection
  * 
@@ -36,6 +108,64 @@ void objectsCallback(const perception::ObjectArray& objs)
 {
     const std::lock_guard<std::mutex> lock(g_objects_mutex); 
     g_objects = objs;
+}
+
+
+/**
+ * @brief Actual algorithm for driving the spiral motion
+ * 
+ */
+void driveSprial()
+{
+  if(g_spiral_points.size()>=3)
+  {
+    double dist = NavigationAlgo::changeInPosition(g_robot_pose, g_spiral_points.at(1));
+    bool done_driving = g_client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED;
+    g_new_trajectory = false;
+    if(g_going_to_goal && !done_driving)
+    {
+      g_last_dist = dist;
+      ros::Duration(0.1).sleep();
+      return;
+      g_going_to_goal = true;
+    }
+    else
+      g_going_to_goal = false;
+
+    if (dist < CHECKPOINT_THRESHOLD)
+    {    
+      geometry_msgs::PointStamped center_of_rot = getCenterOfRotation(g_spiral_points);
+      g_spiral_points.erase(g_spiral_points.begin());
+      g_nav_goal.drive_mode = NAV_TYPE::REVOLVE;
+      g_nav_goal.point = center_of_rot;
+      g_nav_goal.forward_velocity = 2.0;
+      ROS_WARN_STREAM("Circular Motion radius: "<<center_of_rot.point.y<<"\tdist: "<<dist);
+      g_last_dist = 100; // Just to make it big for the next iteration
+      g_new_trajectory = true;
+    }
+    else if (dist > g_last_dist)
+    {
+      geometry_msgs::Point point_0, point_2;
+      point_0 = g_spiral_points.at(0).point;
+      point_2 = g_spiral_points.at(2).point;
+
+      ROS_WARN_STREAM("Going to goal dist:"<<dist);
+      
+      g_nav_goal.drive_mode = NAV_TYPE::GOAL;
+      g_nav_goal.pose.pose.position = g_spiral_points.at(1).point;
+      g_nav_goal.pose.pose.orientation = getOrientation(point_0, point_2);
+      g_nav_goal.pose.header.frame_id = MAP;
+      // g_client->sendGoal(g_nav_goal);
+      g_going_to_goal = true;
+      g_new_trajectory = true;
+    }
+    else
+    {
+      ROS_WARN_STREAM("Dist:"<<dist<<"\tLast Dist:"<<g_last_dist);
+      g_last_dist = dist;
+      g_new_trajectory = false;
+    }
+  }
 }
 
 /**
@@ -56,17 +186,17 @@ void spiralSearch()
 
     if(abs(direction) > 0.0)
     {
+        g_nav_goal.drive_mode = NAV_TYPE::MANUAL;
         g_nav_goal.forward_velocity = FORWARD_VELOCITY;
         g_nav_goal.direction = direction;
         g_nav_goal.angular_velocity = 0;
+        g_new_trajectory = true;
         ROS_INFO("Avoiding Obstacle");
         return;
     }
     else
     {
-        g_nav_goal.forward_velocity = FORWARD_VELOCITY;
-        g_nav_goal.direction = 0;
-        ROS_INFO("Driving Straight");
+        driveSprial();
     }
 }
 
@@ -80,8 +210,9 @@ void execute()
     while (ros::ok())
     {
         spiralSearch();
+        if(g_new_trajectory)
+          g_client->sendGoal(g_nav_goal);
         update_rate.sleep();
-        g_client->sendGoal(g_nav_goal);
         ros::spinOnce();
     }
 }
@@ -98,8 +229,22 @@ int main(int argc, char** argv)
     ros::init(argc, argv, robot_name + COMMON_NAMES::SCOUT_SEARCH_NODE_NAME);
     ros::NodeHandle nh;
 
-    g_nav_goal.drive_mode = COMMON_NAMES::NAV_TYPE::MANUAL;
+    ros::Subscriber odom_sub = nh.subscribe(COMMON_NAMES::CAPRICORN_TOPIC + robot_name + COMMON_NAMES::CHEAT_ODOM_TOPIC, 1000, updateRobotPose);
+
     g_client = new Client(COMMON_NAMES::CAPRICORN_TOPIC + robot_name + "/" + COMMON_NAMES::NAVIGATION_ACTIONLIB, true);
+    g_client->waitForServer();
+    g_nav_goal.drive_mode = COMMON_NAMES::NAV_TYPE::MANUAL;
+
+    geometry_msgs::PointStamped zero_point;
+    zero_point.header.frame_id = MAP;
+    g_spiral_points = NavigationAlgo::getNArchimedeasSpiralPoints(zero_point, 400, 17);
+    
+    while (ros::ok() && !cb_init)
+    {
+      ros::Duration(0.1).sleep();
+      ROS_WARN_STREAM(cb_init);
+      ros::spinOnce();
+    }
 
     ros::Subscriber objects_sub = nh.subscribe(COMMON_NAMES::CAPRICORN_TOPIC + robot_name + COMMON_NAMES::OBJECT_DETECTION_OBJECTS_TOPIC, 1, &objectsCallback);
 
--- utils/include/utils/common_names.h
+++ utils/include/utils/common_names.h
@@ -1,3 +1,5 @@
+#pragma once
+
 namespace COMMON_NAMES
 {
   /****** ROBOTS ******/
