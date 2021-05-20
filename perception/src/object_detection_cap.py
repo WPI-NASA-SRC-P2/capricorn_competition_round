1acd4770354a34c89bb3b067c7b4a53f3b7ca192
jibrangit <jibran.university@gmail.com>
2021-05-20 14:49:31 -0400

    sync odometry tested for both scout and excavator

--- perception/src/object_detection_cap.py
+++ perception/src/object_detection_cap.py
@@ -43,7 +43,7 @@ import numpy as np
 
 physical_devices = tf.config.list_physical_devices('GPU') 
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-tf.config.experimental.set_virtual_device_configuration(physical_devices[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=100)])
+tf.config.experimental.set_virtual_device_configuration(physical_devices[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=800)])
 
 K = [381.36246688113556, 0.0, 320.5, 0.0, 381.36246688113556, 240.5, 0.0, 0.0, 1.0]
 Fx = K[0]
