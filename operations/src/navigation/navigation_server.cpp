31e43be651c76beb0a16cd0e7e828fe5fbbcd116
Dennis Chavez <dennis.chavezromero@hotmail.com>
2021-06-09 12:48:20 -0400

    Update navigation_server.cpp

--- operations/src/navigation/navigation_server.cpp
+++ operations/src/navigation/navigation_server.cpp
@@ -22,7 +22,7 @@ NavigationServer::NavigationServer(ros::NodeHandle& nh, std::string robot_name)
 
 	listener_ = new tf2_ros::TransformListener(buffer_);
 
-	// Initialize the rate limiter to 100 HZ
+	// Initialize the rate limiter to 10 HZ
 	update_rate_ = new ros::Rate(10);
 
 	moveRobotWheels(0);
