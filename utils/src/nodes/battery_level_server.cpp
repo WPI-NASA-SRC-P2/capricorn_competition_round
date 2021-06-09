9c7b142f278a9b67e525403d6f929bc9103a3a32
Alex Galvan <adgalvan@wpi.edu>
2021-06-08 22:14:44 -0400

    Finished battery level calculations

--- utils/src/nodes/battery_level_server.cpp
+++ utils/src/nodes/battery_level_server.cpp
@@ -1,27 +1,41 @@
 #include "battery_level_server.h"
+#include "battery_level.h"
 
 #include <cspace.h>
 #include <astar.h>
 #include "planning/TrajectoryWithVelocities.h"
 #include <geometry_msgs/Point.h>
+#include <nav_msgs/Odometry.h>
 
-
-float distance = 0;
-float current_location = 0;
 
 //Setting the node's update rate
 #define UPDATE_HZ 10
 
-void BatteryLevelServer::poseCallback(geometry_msgs::PoseStamped pose){
+void BatteryLevelServer::poseCallback(nav_msgs::Odometry odom){
+
+    current_location_ = odom.pose.pose;
 
     // calculate the distance between the robot's current location and charging station
-    distance = calc_distance(pose, current_location);         //needs to be calculated using a distance formula
+    distance_ = battery_level::calc_distance(target_location_.pose.position.x, target_location_.pose.position.y, current_location_.pose.position.x, current_location_.pose.position.y)        //needs to be calculated using a distance formula
 }
 
 void BatteryLevelServer::deadlinesCalculator(){
+    std_msgs::Float64MultiArray deadlines;
+
     // publish the soft and hard deadlines to a topic
+    float percentage_needed = battery_level::base_battery_level(battery_level::discharge_rate, BatteryLevelServer::distance_, battery_level::speed);
+    soft_deadline_ = battery_level::calc_soft_deadline(percentage_needed);
+    hard_deadline_ = battery_level::calc_hard_deadline(percentage_needed);
     
-    debug_deadlinesPublisher.pub(); //publish the dealines
+
+   deadlines.dim[0].label  = "soft deadline";
+   deadlines.dim[0].size   = 1;
+   deadlines.dim[1].label  = "hard deadline";
+   deadlines.dim[1].size   = 1;
+   deadlines.data[0] = soft_deadline_;
+   deadlines.data[1] = hard_deadline_;
+    //publish the dealines
+    debug_deadlinesPublisher.pub(deadlines); 
 }
 
 
@@ -34,8 +48,8 @@ int main(int argc, char *argv[])
 
   std::string robot_name_ = robot_name;
 
-  //ROS Topic names
-  std::string pose_topic_ = "/capricorn/" + robot_name_ + "/object_detection_map";
+  //ROS Topic for getting the curren location of the robot
+  std::string pose_topic_ = "/" + robot_name_ + "/camera/odom";
 
   //create a nodehandle
   ros::NodeHandle nh;
@@ -44,7 +58,7 @@ int main(int argc, char *argv[])
 
   server.pose_subscriber = nh.subscribe(pose_topic_, 1000, &BatteryLevelServer::poseCallback, &server);
 
-  debug_deadlinesPublisher = nh.advertise<nav_msgs::OccupancyGrid>("/galaga/batteryLevelDeadlines", 1000);
+  debug_deadlinesPublisher = nh.advertise<std_msgs::Float64MultiArray>("/galaga/batteryLevelDeadlines", 1000);
 
 
   //Instantiating ROS server for calculating deadlines
