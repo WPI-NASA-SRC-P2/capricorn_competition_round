b8ae9f8774c3e11ec0c208ba4165bce5a77d0c4e
Jibrangit <jibran.university@gmail.com>
2021-07-02 22:03:09 -0400

    implemented a lowpass filter

--- utils/src/odom_resetter.cpp
+++ utils/src/odom_resetter.cpp
@@ -20,6 +20,11 @@ bool odom_message_received = false;
 nav_msgs::Odometry global_odometry;
 sensor_msgs::Imu global_imu;
 
+double roll_avg;
+double pitch_avg;
+double yaw_avg;
+int reading_count = 0;
+
 void odom_callback(nav_msgs::Odometry odom_data) 
 {   
     global_odometry = odom_data;
@@ -43,7 +48,25 @@ void getInitRPY(double &init_r, double &init_p, double &init_y)
     
     tf2::Matrix3x3 init_m(init_tf_quat);
     init_m.getRPY(init_r, init_p, init_y);
+
+    // Set the initial values of rpy data
+    roll_avg = init_r;
+    pitch_avg = init_p;
+    yaw_avg = init_y;
+    reading_count++;
 }
+
+void updateAverages(double r, double p, double y){
+
+    roll_avg = (roll_avg * (reading_count - 1) + r) / reading_count;
+    pitch_avg = (pitch_avg * (reading_count - 1) + p) / reading_count;
+    yaw_avg = (yaw_avg * (reading_count - 1) + y) / reading_count;
+}
+
+double lowPassExponential(double input, double average, double factor) 
+{  
+    double retVal = input*factor + (1-factor)*average;  // ensure factor belongs to  [0,1]
+} 
 
 int main(int argc, char** argv)
 {
@@ -56,14 +79,15 @@ int main(int argc, char** argv)
     ros::Subscriber imu_odom_sub = nh.subscribe("/"+ robot_name + "/imu", 223, imu_callback);
 
     ros::ServiceClient reset_odom_to_pose_client = nh.serviceClient<rtabmap_ros::ResetPose>(robot_name + COMMON_NAMES::RESET_POSE_CLIENT);
-    // ros::ServiceClient rtabmap_client = nh.serviceClient<rtabmap_ros::ResetPose>(target_name + RESET_POSE_CLIENT);
 
-    while(ros::ok() && (!imu_message_received && !odom_message_received))
+    // Wait until we have received a message from both odom and imu
+    while(ros::ok() && !imu_message_received && !odom_message_received)
     {
         ros::Duration(0.1).sleep();
         ros::spinOnce();
     }
 
+    // Get the initial rpy values from the odom
     double init_r, init_p, init_y;
     do
     {
@@ -71,40 +95,45 @@ int main(int argc, char** argv)
         ros::spinOnce();
     } while ((init_r != init_r || init_p != init_p || init_y != init_y) && ros::ok());
     
-
+    // Inf loop
     while(ros::ok())
     {
-        //get position from odom
-        geometry_msgs::Point position = global_odometry.pose.pose.position;
+        //get current position from odom
+        geometry_msgs::Point curr_odom_position = global_odometry.pose.pose.position;
         
-        //get orientation from imu as a quat
+        //get current orientation from imu as a quat
         geometry_msgs::Quaternion quat = global_imu.orientation;
         tf2::Quaternion tf_quat(quat.x,
                                 quat.y,
                                 quat.z,
                                 quat.w);
 
-        // convert current orientation to rpy
+        // convert current orientation from quat to rpy
         tf2::Matrix3x3 m(tf_quat);
         double r, p, y;
         m.getRPY(r, p, y);
 
-        // add init + current rpy
+        reading_count++;
+        
+        // Use low pass filter to get new filtered values of rpy
         double new_r, new_p, new_y;
-        new_r = init_r + r;
-        new_p = init_p + p;
-        new_y = init_y + y;
+        new_r = lowPassExponential(r, roll_avg, 0.2);
+        new_p = lowPassExponential(p, pitch_avg, 0.2);
+        new_y = lowPassExponential(y, yaw_avg, 0.2);
+
+        // Update averages for future filtering
+        updateAverages(r, p, y);   
 
         // Assemble data into pose for Rosservice call 
         rtabmap_ros::ResetPose pose;
-        pose.request.x = position.x;
-        pose.request.y = position.y;
-        pose.request.z = position.z;
+        pose.request.x = curr_odom_position.x;
+        pose.request.y = curr_odom_position.y;
+        pose.request.z = curr_odom_position.z;
         pose.request.roll = new_r;
         pose.request.pitch = new_p;
         pose.request.yaw = new_y;
 
-        // call the 'reset pose' rosservice
+        // call the 'reset pose' rosservice to send updated data
         if(reset_odom_to_pose_client.call(pose)){
             ROS_INFO_STREAM("[UTILS | odom_resetter.cpp | " + robot_name + "]: " + "Pose initialized for rtabmap");
         }
@@ -112,6 +141,7 @@ int main(int argc, char** argv)
             ROS_ERROR_STREAM("[UTILS | odom_resetter.cpp | " + robot_name + "]: " + "RTabMap initialize pose failed.");
         }
 
+        // Wait 10 seconds and then do it all again
         ros::Duration(10).sleep();
         ros::spinOnce();
     }
