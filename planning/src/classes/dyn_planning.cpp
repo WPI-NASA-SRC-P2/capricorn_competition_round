d1a85b62c3dc3f38d9f5630ac1bad320aae08e15
chrisdemaio <rdpatil@wpi.edu>
2021-07-07 17:42:27 -0400

    Saved all the indexes of the path,n more - Mandar

--- planning/src/classes/dyn_planning.cpp
+++ planning/src/classes/dyn_planning.cpp
@@ -1,18 +1,39 @@
 #include "dyn_planning.h"    
 #include "astar.h"
+using namespace std;
 
-    
-bool DynamicPlanning::checkForObstacles(nav_msgs::Path& path, nav_msgs::OccupancyGrid& oGrid)
+
+float lengthOfPath(int path[])
 {
- 
-    for (int i = 0; i < path.poses.size(); i++)
-    {
-        if(oGrid.data[AStar::indexFromPoseStamped(path.poses[i].pose.position, oGrid)] > 50)
-        {
-            ROS_INFO("Obstacle on the current path");
-            return true;
-        }
-    }
+    start_point = path[0];
+    end_point = path[path.size() - 1];
+    return std::sqrt(pow(start_point[0] - end_point[0], 2) + pow(start_point[1] - end_point[1], 2));
+}
 
-    return false;
+bool DynamicPlanning::checkForObstacles(nav_msgs::Path& path, nav_msgs::OccupancyGrid& oGrid) // path is giving the grid coordinates
+{
+ /* give out the number of points till 6 m in the path 
+    to be checked for their threshold values, 
+    not just the wavepoints that the path is returning 
+ */
+
+    std::vector<int>Path_index;
+    if(lengthOfPath(nav::msgs Path& path) > 6)    // it is needed because we cant decide from the number of waypoints.   
+        for (int i = 0; i < path.poses.size(); i++)
+        {   
+            Path_index.push_back(oGrid.data[AStar::indexFromPoseStamped(path.poses[i].pose.position, oGrid)]);
+            std::reverse_iterator(Path_index.rbegin(), Path_index.rend()); // saved all the index points from start point to end point.
+            
+        
+
+
+            if(oGrid.data[AStar::indexFromPoseStamped(path.poses[i].pose.position, oGrid)] > 50) // first converting the grid coordinate to the index  
+            {                                                                                  // and then checking the threshold value of that index 
+                ROS_INFO("Obstacle on the current path");
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
