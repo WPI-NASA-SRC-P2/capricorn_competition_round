b47a813ddf89062f583f9b8ee9e27f3b6b44d82f
Jibrangit <jibran.university@gmail.com>
2021-07-08 16:36:36 -0400

    Added insignificant comment
    Co-authored-by: aenyedy56 <aenyedy56@gmail.com>

--- state_machines/src/scout/scout_state_machine.cpp
+++ state_machines/src/scout/scout_state_machine.cpp
@@ -352,7 +352,7 @@ void ResetOdomAtHopper::resetOdom()
    return;
 }
 
-//Go to repair station after resetting odometry.
+//Go to repair station after resetting 
 void ResetOdomAtHopper::goToRepair()
 {
    if(first_GTR)
